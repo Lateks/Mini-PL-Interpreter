e9873c9dce14b437f95380f56ade9a302ff83c9c
Laura Leppanen <laura.leppanen@iki.fi>
2012-03-08 01:05:01 +0200

    Visitor interface for traversing the AST.

---
+++ MiniPLInterpreter/NodeVisitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AST;
+
+namespace MiniPlInterpreter
+{
+    interface NodeVisitor
+    {
+        void visit(Program node);
+        void visit(IntegerLiteral node);
+        void visit(StringLiteral node);
+        void visit(VariableDeclaration node);
+        void visit(Variable node);
+        void visit(Keyword node);
+        void visit(BinaryOp node);
+        void visit(UnaryNot node);
+        void visit(Loop node);
+        void visit(Range node);
+        void visit(Assignment node);
+        void visit(ExpressionStatement node);
+        void visit(ReadStatement node);
+    }
+}
