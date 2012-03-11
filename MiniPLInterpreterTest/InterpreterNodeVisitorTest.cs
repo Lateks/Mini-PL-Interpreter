a6e0d681c25c017994f0884044d04fb99b420c4c
Laura Leppanen <laura.leppanen@iki.fi>
2012-03-12 00:07:33 +0200

    Test logical operations in interpreter visitor.

--- MiniPLInterpreterTest/InterpreterNodeVisitorTest.cs
+++ MiniPLInterpreterTest/InterpreterNodeVisitorTest.cs
@@ -206,4 +206,123 @@ namespace MiniPLInterpreterTest
             Assert.Throws<AssertionFailed>(() => interpreter.run(new Program(program)));
         }
     }
+
+    [TestFixture]
+    class LogicalOpTest
+    {
+        SymbolTable symboltable;
+        List<Statement> program;
+        VariableDeclaration result;
+        InterpretingNodeVisitor interpreter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            program = new List<Statement>();
+            symboltable = new SymbolTable();
+            symboltable.define(new Symbol("result", "bool"));
+            result = new VariableDeclaration("result", "bool");
+            interpreter = new InterpretingNodeVisitor(symboltable);
+        }
+
+        [Test]
+        public void StringEquals()
+        {
+            var equals = new LogicalOp("=", new StringLiteral("foo"), new StringLiteral("foo"));
+            var assignment = new Assignment(result, equals);
+            program.Add(assignment);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(true));
+        }
+
+        [Test]
+        public void StringDoesNotEqual()
+        {
+            var equals = new LogicalOp("=", new StringLiteral("bar"), new StringLiteral("foo"));
+            var assignment = new Assignment(result, equals);
+            program.Add(assignment);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IntEquals()
+        {
+            var equals = new LogicalOp("=", new IntegerLiteral("5"), new IntegerLiteral("5"));
+            var assignment = new Assignment(result, equals);
+            program.Add(assignment);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IntDoesNotEqual()
+        {
+            var equals = new LogicalOp("=", new IntegerLiteral("4"), new IntegerLiteral("5"));
+            var assignment = new Assignment(result, equals);
+            program.Add(assignment);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(false));
+        }
+
+        [Test]
+        public void BoolEquals()
+        {
+            var boolean = new LogicalOp("=", new IntegerLiteral("4"), new IntegerLiteral("5"));
+            var equals = new LogicalOp("=", boolean, boolean);
+            var assignment = new Assignment(result, equals);
+            program.Add(assignment);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(true));
+        }
+
+        [Test]
+        public void BoolDoesNotEqual()
+        {
+            var falseboolean = new LogicalOp("=", new IntegerLiteral("4"), new IntegerLiteral("5"));
+            var trueboolean = new LogicalOp("=", new IntegerLiteral("5"), new IntegerLiteral("5"));
+            var equals = new LogicalOp("=", falseboolean, trueboolean);
+            var assignment = new Assignment(result, equals);
+            program.Add(assignment);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(false));
+        }
+
+        [Test]
+        public void FalseAnd()
+        {
+            var falseboolean = new LogicalOp("=", new IntegerLiteral("4"), new IntegerLiteral("5"));
+            var trueboolean = new LogicalOp("=", new IntegerLiteral("4"), new IntegerLiteral("5"));
+            var and1 = new LogicalOp("&", falseboolean, trueboolean);
+            var and2 = new LogicalOp("&", falseboolean, falseboolean);
+            var assignment1 = new Assignment(result, and1);
+            program.Add(assignment1);
+            var result2 = new VariableDeclaration("result2", "bool");
+            symboltable.define(new Symbol("result2", "bool"));
+            var assignment2 = new Assignment(result2, and2);
+            program.Add(assignment2);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(false));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result2")], Is.EqualTo(false));
+        }
+
+        [Test]
+        public void TrueAnd()
+        {
+            var trueboolean = new LogicalOp("=", new IntegerLiteral("4"), new IntegerLiteral("4"));
+            var and = new LogicalOp("&", trueboolean, trueboolean);
+            var assignment = new Assignment(result, and);
+            program.Add(assignment);
+
+            interpreter.run(new Program(program));
+            Assert.That(interpreter.Valuetable[symboltable.resolve("result")], Is.EqualTo(true));
+        }
+    }
 }
