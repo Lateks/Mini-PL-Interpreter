d60dfce0e1a7f2c879c7855292fb71d376e6eb03
Laura Leppanen <laura.leppanen@iki.fi>
2012-03-16 18:35:51 +0200

    Add error descriptions.

--- MiniPLInterpreter/Errors.cs
+++ MiniPLInterpreter/Errors.cs
@@ -5,30 +5,37 @@ using System.Text;
 
 namespace Errors
 {
+    // Lexical errors are thrown by the scanner.
     public class LexicalError : Exception
     {
         public LexicalError(string message)
             : base(message) { }
     }
 
+    // Syntax errors are thrown by the parser.
     public class SyntaxError : Exception
     {
         public SyntaxError(string message)
             : base(message) { }
     }
 
+    // Semantic errors are thrown by the typechecker.
     public class SemanticError : Exception
     {
         public SemanticError(string message)
             : base(message) { }
     }
 
+    // Mini-PL internal error (thrown by interpreter
+    // visitor).
     public class MiniPLAssertionFailed : Exception
     {
         public MiniPLAssertionFailed(string message)
             : base(message) { }
     }
 
+    // Mini-PL internal error (thrown by interpreter
+    // visitor).
     public class MiniPLReadError : Exception
     {
         public MiniPLReadError(string message)
